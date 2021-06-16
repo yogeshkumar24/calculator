0b83f8948e42942255940e16741d1dbba1b2e7dd
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2021-06-16 22:08:12 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'dart:collection';
+
+import 'package:calculator/stack/stack.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main(){
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  @override
+  State createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator>{
+
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Calculator')),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.all(20),
+            child: Text(equation,),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.all(20),
+            child: Text(result, ),
+          ),
+          Expanded(child: Divider(),
+
+          ),
+          Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("C", ),
+                        buildButton("DEL",),
+                        buildButton("/",),
+                        buildButton("*",),
+                      ]
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("7",),
+                        buildButton("8",),
+                        buildButton("9",),
+                        buildButton("-",),
+                      ]
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("4",),
+                        buildButton("5",),
+                        buildButton("6",),
+                        buildButton("+",),
+                      ]
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("1",),
+                        buildButton("2",),
+                        buildButton("3",),
+                        buildButton("=",),
+                      ]
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("Undo",),
+                        buildButton("0",),
+                        buildButton("Redo",),
+                      ]
+                  ),
+                ],
+              ),
+            ],
+          ),
+      );
+
+  }
+
+  Widget buildButton(String buttonText){
+    return Container(
+      color: Colors.green,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              side: BorderSide(
+                  color: Colors.white,
+                  width: 1,
+                  style: BorderStyle.solid )
+          ),
+          padding: EdgeInsets.all(16.0),
+          onPressed: () => onButtonPress(buttonText),
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+          )),
+    );
+  }
+
+
+  onButtonPress(String buttonText) {
+    setState(() {
+      if(buttonText == "C"){
+        equation = "0";
+        result = "0";
+
+      }
+      else  if(buttonText == "Redo"){
+        String ele = Stack().pop();
+
+      }else if(buttonText == "DEL"){
+        List list = equation.split("");
+
+       Stack().push(list.last);
+       print(list.last);
+        equation = equation.substring(0,equation.length -1);
+        if(equation == ""){
+          equation = "0";
+        }
+      }else if(buttonText == "="){
+        expression = equation;
+        try{
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = "${exp.evaluate(EvaluationType.REAL, cm)}";
+        }catch(e){
+          result = "error";
+        }
+
+      }else {
+        if(equation == "0"){
+          equation = buttonText;
+        }else{
+          equation = equation + buttonText;
+        }
+      }
+    });
+  }
+}
+
+class Stack<String> {
+  final _stack = Queue<String>();
+
+  void push(String element) {
+    _stack.addLast(element);
+  }
+
+  String pop() {
+    final String lastElement = _stack.last;
+    _stack.removeLast();
+    return lastElement;
+  }
+
+  void clear() {
+    _stack.clear();
+  }
+
+  bool get isEmpty => _stack.isEmpty;
+}
